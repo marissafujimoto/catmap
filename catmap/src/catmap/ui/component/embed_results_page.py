7d3bf7ee91d3154eff3ac1deabf2263ab3e76fa8
marissafujimoto <fujim@uw.edu>
2025-03-18 13:35:43 -0700

    Show results of embedding calculation

--- catmap/src/catmap/ui/component/embed_results_page.py
+++ catmap/src/catmap/ui/component/embed_results_page.py
@@ -6,9 +6,7 @@ from tempfile import NamedTemporaryFile
 from streamlit.delta_generator import DeltaGenerator
 
 from catmap.ui.component.embedding_plotter import EmbeddingPlotter
-from catmap.ui.component.select_column_dropdown import SelectColumnDropdown
 from catmap.ui.component.abstract_component import AbstractUIComponent
-from catmap.ui.component.info_button import InfoButton
 from catmap.ui.component.return_home_button import ReturnHomeButton
 from catmap.io.model_loader import embed_nsclc_data
 
@@ -21,17 +19,18 @@ class EmbedResultsPage(AbstractUIComponent):
         with col1:
             uploaded_file = parent.file_uploader(
                 "Select h5ad File", accept_multiple_files=False)
-            print(uploaded_file.name)
-
-            if uploaded_file:
-                with NamedTemporaryFile(dir='.', suffix='.h5ad') as f:
-                    f.write(uploaded_file.getbuffer())
-
-                    embedding_df = embed_nsclc_data(f.name)
-                    # TODO plot the embeddings
-
         with col2:
             pass
+
+        if uploaded_file:
+            with NamedTemporaryFile(dir='.', suffix='.h5ad') as f:
+                f.write(uploaded_file.getbuffer())
+
+                embedding_df = embed_nsclc_data(f.name)
+
+            embedding_plotter = EmbeddingPlotter(
+                embedding_df, "Predicted Cell Type", "UMAP1", "UMAP2")
+            embedding_plotter.build(parent)
 
         with parent.expander("About Embedding"):
             # TODO: expand on this writing
